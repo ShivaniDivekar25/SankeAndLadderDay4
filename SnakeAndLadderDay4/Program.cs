cd55c73e42f4d149fcd7edea9330516dc4f9aef8
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 20:14:08 +0530

    Modified to Dice Roll UC2 program

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -5,8 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome! to Snake and Ladder Game");
-            int position = 0;       //Position Initializing
-            Console.WriteLine("The position of player starts: " + position);
+            int position = 0;       //Position Initializin
+            Random random = new Random();       //Creating object random
+            int Dice = random.Next(1, 7);       // Using random method 
+            Console.WriteLine("The postition of player starts: {0} and random number of Dice: {1}",position, Dice);
         }
     }
 }
