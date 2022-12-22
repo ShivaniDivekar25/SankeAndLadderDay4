e5f55ee124de3b1261302b0a8f139310da42a515
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 21:01:16 +0530

    Modifed to get Player Win Conditon(upto 100)

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -5,26 +5,31 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome! to Snake and Ladder Game");
-            int position = 0;       //Position Initializin
+            int position = 0;
             Random random = new Random();       //Creating object random
-            int Dice = random.Next(1, 7);       // Using random method 
-            Console.WriteLine("The postition of player starts: {0} and random number of Dice: {1}",position, Dice);
-            Random random1 = new Random();      //Creating object random
-            int option = random1.Next(1, 4);        //Using random method
+           // Random random1 = new Random();      //Creating object random
 
-            switch (option)         //Checking for options using switch case
+            while(position<=100)
             {
-                case 1:
-                    position = position + Dice;
-                    Console.WriteLine("The player moves ahead by number Dice value " + position);
-                    break;
-                case 2:
-                    position = position - Dice;
-                    Console.WriteLine("The player moves behind by number Dice value " + position);
-                    break;
-                default:
-                    Console.WriteLine("The player stay at same place " + position);
-                    break;
+                int Dice = random.Next(1, 7);
+                int Option = random.Next(1, 4);
+
+                switch (Option)         //Checking for options using switch case
+                {
+                    case 1:
+                        position = position + Dice;     //Updating position
+                        Console.WriteLine("The player moves ahead by number Dice value " + position);
+                        break;
+                    case 2:
+                        position = position - Dice;     //Updating position
+                        if (position < 0)       //Condition to get exact 100
+                            position = 0;
+                        Console.WriteLine("The player moves behind by number Dice value " + position);
+                        break;
+                    default:
+                        Console.WriteLine("The player stay at same place " + position);
+                        break;
+                }
             }
         }
     }
