1d5b02343eee077c228ed0d3583c96cb0f7ad0e4
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 20:04:48 +0530

    Modified to Position Zero of playerUC1

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome! to Snake and Ladder Game");
+            int position = 0;       //Position Initializing
+            Console.WriteLine("The position of player starts: " + position);
         }
     }
 }
