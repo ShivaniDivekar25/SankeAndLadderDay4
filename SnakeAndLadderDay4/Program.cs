d444d18b4f738d59c6d10b9e4a34fb9a1e7706f0
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 20:24:17 +0530

    Modified to Checking for Options UC3

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -9,6 +9,23 @@
             Random random = new Random();       //Creating object random
             int Dice = random.Next(1, 7);       // Using random method 
             Console.WriteLine("The postition of player starts: {0} and random number of Dice: {1}",position, Dice);
+            Random random1 = new Random();      //Creating object random
+            int option = random1.Next(1, 4);        //Using random method
+
+            switch (option)         //Checking for options using switch case
+            {
+                case 1:
+                    position = position + Dice;
+                    Console.WriteLine("The player moves ahead by number Dice value " + position);
+                    break;
+                case 2:
+                    position = position - Dice;
+                    Console.WriteLine("The player moves behind by number Dice value " + position);
+                    break;
+                default:
+                    Console.WriteLine("The player stay at same place " + position);
+                    break;
+            }
         }
     }
 }
