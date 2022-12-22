49a900c64145c1217a3541069fa8d5f0cbcd396f
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 19:58:52 +0530

    Welcome to Snake and Ladder Game program

---
+++ SnakeAndLadderDay4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadderDay4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! to Snake and Ladder Game");
+        }
+    }
+}
