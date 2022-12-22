986c30d06fd1273b41a34b1a8b82716fb7dc9288
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-22 21:12:34 +0530

    Modified to get Exact Winning Condition

--- SnakeAndLadderDay4/Program.cs
+++ SnakeAndLadderDay4/Program.cs
@@ -7,9 +7,8 @@
             Console.WriteLine("Welcome! to Snake and Ladder Game");
             int position = 0;
             Random random = new Random();       //Creating object random
-           // Random random1 = new Random();      //Creating object random
-
-            while(position<=100)
+             
+            while(position<100)
             {
                 int Dice = random.Next(1, 7);
                 int Option = random.Next(1, 4);
@@ -18,16 +17,18 @@
                 {
                     case 1:
                         position = position + Dice;     //Updating position
-                        Console.WriteLine("The player moves ahead by number Dice value " + position);
+                        if (position > 100)     //Updating to get 100
+                            position = 100;
+                        Console.WriteLine("The player is got ladder {0} and palyer moves ahead by number Dice value {1} ",Dice, position);
                         break;
                     case 2:
                         position = position - Dice;     //Updating position
-                        if (position < 0)       //Condition to get exact 100
+                        if (position < 0)       //Condition to get exact 0
                             position = 0;
-                        Console.WriteLine("The player moves behind by number Dice value " + position);
+                        Console.WriteLine("The player is got snake {0} and player moves behind by number Dice value ",Dice, position);
                         break;
                     default:
-                        Console.WriteLine("The player stay at same place " + position);
+                        Console.WriteLine(" NO play,{0}.The player stay at same place {1} " ,Dice ,position);
                         break;
                 }
             }
